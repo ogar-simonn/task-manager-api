64b23886e5b31c8efdf537582442186075031486
Ogar Simon <ogarsimon0000@gmail.com>
2023-01-12 16:28:07 +0100

    bug fixes

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ const errorHandlerMiddleWare = require("./middleware/errorHandler");
 const authRouter = require("./routes/auth");
 const authentication = require("./middleware/authentication");
 
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 require("dotenv").config();
 
 app.use([
